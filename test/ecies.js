068f9c777ac9d0ffffa6bf13d596bd560f05592f
Justin Silver <jsilver@doublesharp.com>
2018-06-02 12:40:34 -0700

    oldnodecode

--- test/ecies.js
+++ test/ecies.js
@@ -51,7 +51,7 @@ describe('ECIES', function() {
     .publicKey(aliceKey.publicKey);
 
   var message = 'attack at dawn';
-  var encrypted = `0339e504d6492b082da96e11e8f039796b06cd4855c101e2492a6f10f3e056a9e712c732611c6917ab5c57a1926973bc44a1586e94a783f81d05ce72518d9b0a80e2e13c7ff7d1306583f9cc7a48def5b37fbf2d5f294f128472a6e9c78dede5f5`;
+  var encrypted = '0339e504d6492b082da96e11e8f039796b06cd4855c101e2492a6f10f3e056a9e712c732611c6917ab5c57a1926973bc44a1586e94a783f81d05ce72518d9b0a80e2e13c7ff7d1306583f9cc7a48def5b37fbf2d5f294f128472a6e9c78dede5f5';
   var encBuf = new Buffer(encrypted, 'hex');
 
   it('correctly encrypts a message', function() {
